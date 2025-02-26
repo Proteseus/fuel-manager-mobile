e74fc9d2fa783860a3f724cbbac3c9becaf35fae
Proteseus <lewiwui@gmail.com>
2025-02-26 10:50:44 +0300

    eas conf

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,5 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  extra: {
-    apiUrl: process.env.EXPO_PUBLIC_API_URL,
-  },
+  ...config
 });
