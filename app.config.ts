33b20acbaf25cd7a1fd6eef53529a16ababa7170
Proteseus <lewiwui@gmail.com>
2025-03-24 21:01:47 +0300

    config switch

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,49 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config
+  ...config,
+  name: "Fuel Manager",
+  slug: "fueler",
+  owner: "debrye",
+  version: "0.6.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    "supportsTablet": true
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#ffffff"
+    },
+    package: "com.lewibelayneh.fueler",
+    versionCode: 1
+  },
+  web: {
+    favicon: "./assets/icon.png",
+    bundler: "metro"
+  },
+  plugins: [
+    "expo-router"
+  ],
+  scheme: "fuel-manager",
+  extra: {
+    apiUrl: "https://fueler.mssaba.com/api",
+    router: {
+      origin: false
+    },
+    eas: {
+      projectId: "8c2a15d4-d3d6-47df-9bc7-5030b42eba0c"
+    }
+  }
 });
